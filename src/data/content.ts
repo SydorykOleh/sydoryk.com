d2cec359ceae9197759c40919578ae20e1b3959e
Cossack <sydorykoleh@gmail.com>
2026-01-18 13:05:40 +0100

    added configurators

--- src/data/content.ts
+++ src/data/content.ts
@@ -170,6 +170,7 @@ export const filterItems: FilterItem[] = [
 	{ content: "CGI Movies", dataGroup: "movies" },
 	{ content: "Product Animation", dataGroup: "animation" },
 	{ content: "Product Stills", dataGroup: "still" },
+	{ content: "Configurator", dataGroup: "configurator" },
 ];
 
 
